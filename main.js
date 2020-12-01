8fdd401e16e18306e3cb128ea220c0a6b7e4a6e6
fernando duenas <fernando39615@aim.com>
2020-12-01 11:03:10 -0500

    second commit

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const strikeThroughFirstLi = () => {
 }
 // 2
 strikeThroughFirstLi();
-// ****************************************************
+// ************************************************
 
 // 3
 const stringRepId = (id, url) => {
@@ -16,7 +16,7 @@ const stringRepId = (id, url) => {
 stringRepId('#image-1', 'https://media.giphy.com/media/dE5VNLfHyf79K/giphy.gif')
 stringRepId('#image-2', 'https://media.giphy.com/media/sTbavrq6EA3GE/giphy.gif')
 stringRepId('#image-3', 'https://media.giphy.com/media/R2XQJMdRutBZe/giphy.gif')
-// *****************************************************
+// ************************************************
 
 // 5
 const lastLiRemoved = () => {
@@ -27,9 +27,44 @@ const lastLiRemoved = () => {
 lastLiRemoved();
 lastLiRemoved();
 
-// ******************************************************
-// ******************************************************
+// ************************************************
+// ************************************************
 
+// 1
 const newElem = () => {
-    const newImg = document.createElement('img')
+    const newLi = document.createElement('li');
+    newLi.id = 'li-img';
+    const ulElem = document.querySelector('#arguments');
+    ulElem.appendChild(newLi);
+// 2
+    const newImg = document.createElement('img');
+    newImg.id = 'image-4'
+    const liElem = document.querySelector('#li-img');
+    liElem.appendChild(newImg)
+    newImg.src = 'https://media.giphy.com/media/zgItGbJ0vNO0g/giphy.gif'
 }
+newElem();
+
+// 3
+const img30 = (id) => {
+    const imgId = document.querySelector(id);
+    imgId.style.height = '30px'
+}
+// 4
+img30('#image-1')
+// **********************************************
+
+// 5
+const invisible = (elem) => {
+    const invisibleElem = document.querySelector(elem);
+    invisibleElem.className = 'invisible';
+}
+// 6
+invisible('#heading');
+// **********************************************
+
+const reSize = (fontSize, text) => {
+    const resizeText = document.querySelector(text);
+    resizeText.style.fontSize = fontSize;
+}
+reSize('5px', 'p');
